4fd274a948d0c41a51a3e12d8c48a9c0d54ff8a1
jkgiesler <jason.giesler@gmail.com>
2014-03-13 08:35:16 -0500

    still buggy but will send "test" once

--- washerwatcher/src/main/java/com/wash/washerwacher/MainActivity.java
+++ washerwatcher/src/main/java/com/wash/washerwacher/MainActivity.java
@@ -1,10 +1,15 @@
 package com.wash.washerwacher;
 
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import java.io.*;
+import java.net.*;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,12 +17,45 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ugly temporary fix to see other bugs
+        StrictMode.ThreadPolicy policy;
+        policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //getting IO to work on button press
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //EditText text = (EditText)findViewById(R.id.editText);
+                String value = "test";
+                //text.getText().toString();
+                String hostName = "192.168.1.113"; //change to computer ID
+                int portNumber = 5544;
+
+                try {
+                    Socket echoSocket = new Socket(hostName, portNumber);
+                    PrintWriter out =
+                            new PrintWriter(echoSocket.getOutputStream(), true);
+                    out.println(value);
+                } catch (UnknownHostException e) {
+                    System.err.println("Don't know about host " + hostName);
+                    System.exit(1);
+                } catch (IOException e) {
+                    System.err.println("Couldn't get I/O for the connection to " +
+                            hostName);
+                    System.exit(1);
+                }
+            }
+        });
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -35,4 +73,6 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-}
+
+    }
+
